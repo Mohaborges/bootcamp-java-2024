148945e77fab81ae74cd806e619a609d9214f93c
Moha <mohammed.kebdani.cmrm15@gmail.com>
2024-03-13 16:23:51 +0100

    Subiendo ejercicios de JavaScript

--- JavaScript/EjercicioMath/main.js
+++ JavaScript/EjercicioMath/main.js
@@ -4,3 +4,18 @@ random de Math.
 2. Investiga como redondear el valor de Math.Pl 3,1415... a la parte
 decimal 3. */
 
+// Generar un número aleatorio entre 0 y 1
+let numAleatorio = Math.random();
+
+// Redondear el número para obtener 0 o 1
+let result = Math.round(numAleatorio);
+
+console.log(result);
+
+let PI = Math.PI; // Obtener el valor de PI
+
+// Redondear el valor de PI a 3 decimales
+let PIRedondeado = parseFloat(PI.toFixed(0));
+
+console.log(PIRedondeado); // Imprimir el valor redondeado de PI
+
