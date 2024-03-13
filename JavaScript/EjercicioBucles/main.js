1397334dd48733ff4c40f375ae0193a3ffd6191b
Moha <mohammed.kebdani.cmrm15@gmail.com>
2024-03-13 17:15:45 +0100

    Subiendo ejercicio de bucles

---
+++ JavaScript/EjercicioBucles/main.js
@@ -0,0 +1,21 @@
+/*Realiza un bucle tanto con FOR como con WHILE que impriman 10
+veces 10 i ❤️ code */
+
+console.log("Usando bucle FOR:");
+for (let index = 0; index < 10; index++) {
+    console.log((index+1) + " - " + "I ❤️ code!")
+}
+
+console.log("Usando bucle WHILE:");
+let index = 0;
+while (index < 10) {
+    console.log((index+1) + " - " + "I ❤️ code!")
+    index++;
+}
+
+
+
+
+
+
+
